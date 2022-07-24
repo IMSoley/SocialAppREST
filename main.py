c81df4977894c3b3f4c8ff332bd94fa90cf51dc2
Soleyman <soleyman404@gmail.com>
2022-07-25 00:16:24 +0600

    start using pydanctic model

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to My API!!!"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    print(new_post)
+    print(new_post.dict())
+    return {"data": "new post"}
+
