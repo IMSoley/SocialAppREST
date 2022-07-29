837a68773cf6c13a8748b5a9a7f95e6806d254d4
Soleyman <soleyman404@gmail.com>
2022-07-29 14:00:52 +0600

    added: API token system

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from typing import List
-from .. import models, schemas
+from .. import models, schemas, oauth2
 from ..database import get_db
 
 router = APIRouter(
@@ -11,13 +11,13 @@ router = APIRouter(
 
 # get all posts
 @router.get("/", response_model=List[schemas.ResponsePost])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     posts = db.query(models.Post).all()
     return posts
 
 # create a new post
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.ResponsePost)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -26,32 +26,29 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
 
 # get a specific post by id
 @router.get("/{post_id}", response_model=schemas.ResponsePost)
-def get_post(post_id: int, db: Session = Depends(get_db)):
+def get_post(post_id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == post_id).first()
     if not post:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
     return post
 
 # delete a specific post by id
 @router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(post_id: int, db: Session = Depends(get_db)):
+def delete_post(post_id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == post_id)
     if post.first() is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
     post.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 # update a specific post by id
 @router.put("/{post_id}", response_model=schemas.ResponsePost)
-def update_post(post_id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post(post_id: int, post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     post_to_update = db.query(models.Post).filter(models.Post.id == post_id)
     update_post = post_to_update.first()
     if update_post is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
     post_to_update.update(post.dict(), synchronize_session=False)
     db.commit()
     return post_to_update.first()
